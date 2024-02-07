45813cdbebb6c371fd49faf4f9907946c4ba59e9
gpynadath <{gshibu123@gmail.com}>
2024-02-07 11:13:09 +0000

    seperate page for topics

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,9 @@ const Header = () => {
       <Link to="/articles">
         <button>Articles</button>
       </Link>
+      <Link to="/topics">
+        <button>Topics</button>
+      </Link>
     </div>
   );
 };
